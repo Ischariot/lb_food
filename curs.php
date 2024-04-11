c6a8984b634bfb138b4aeb198bb15b8c006b437d
Ischariot <71774369+Ischariot@users.noreply.github.com>
2024-04-12 00:35:46 +0300

    Update curs.php

--- curs.php
+++ curs.php
@@ -16,6 +16,20 @@ class Ingredient {
     public function getPricePerUnit() {
         return $this->price_per_unit * (1 + $this->markup);
     }
+
+    public function saveToFile($filename) {
+        $data = [
+            'name' => $this->name,
+            'price_per_unit' => $this->price_per_unit,
+            'markup' => $this->markup
+        ];
+        file_put_contents($filename, serialize($data));
+    }
+
+    public static function loadFromFile($filename) {
+        $data = unserialize(file_get_contents($filename));
+        return new Ingredient($data['name'], $data['price_per_unit'], $data['markup']);
+    }
 }
 
 class Recipe {
@@ -41,6 +55,26 @@ class Recipe {
             $total_cost += $ingredient->getPricePerUnit();
         }
         return $total_cost;
+    }
+
+    public function saveToFile($filename) {
+        $data = [
+            'name' => $this->name,
+            'ingredients' => []
+        ];
+        foreach ($this->ingredients as $ingredient) {
+            $data['ingredients'][] = $ingredient->saveToFile('');
+        }
+        file_put_contents($filename, serialize($data));
+    }
+
+    public static function loadFromFile($filename) {
+        $data = unserialize(file_get_contents($filename));
+        $ingredients = [];
+        foreach ($data['ingredients'] as $ingredient_data) {
+            $ingredients[] = Ingredient::loadFromFile($ingredient_data);
+        }
+        return new Recipe($data['name'], $ingredients);
     }
 }
 
@@ -72,6 +106,22 @@ class Dish {
     public function calculateCost() {
         return $this->recipe->calculateCost() * (1 + $this->markup) * $this->quantity;
     }
+
+    public function saveToFile($filename) {
+        $data = [
+            'name' => $this->name,
+            'recipe' => $this->recipe->saveToFile(''),
+            'quantity' => $this->quantity,
+            'markup' => $this->markup
+        ];
+        file_put_contents($filename, serialize($data));
+    }
+
+    public static function loadFromFile($filename) {
+        $data = unserialize(file_get_contents($filename));
+        $recipe = Recipe::loadFromFile($data['recipe']);
+        return new Dish($data['name'], $recipe, $data['quantity'], $data['markup']);
+    }
 }
 
 $flour = new Ingredient('flour', 0.5, 0.1); // 10% націнка
@@ -82,4 +132,8 @@ $cookie_recipe = new Recipe('cookie', [$flour, $sugar, $butter]);
 
 $cookie = new Dish('cookie', $cookie_recipe, 12, 0.1); // 10% націнка
 
-echo $cookie->calculateCost();  // Output: 21.78
+$cookie->saveToFile('cookie.txt');
+
+$loaded_cookie = Dish::loadFromFile('cookie.txt');
+
+echo $loaded_cookie->calculateCost();  // Output: 21.78
